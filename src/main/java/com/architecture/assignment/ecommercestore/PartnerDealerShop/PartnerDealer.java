22c98bdc5e628fdf25ca2abd249f9221cc31d396
sandeepa amcp <sandeepaamcp@gmail.com>
2020-01-29 23:39:42 +0530

    initial commit

---
+++ src/main/java/com/architecture/assignment/ecommercestore/PartnerDealerShop/PartnerDealer.java
@@ -0,0 +1,6 @@
+package com.architecture.assignment.ecommercestore.PartnerDealerShop;
+
+public class PartnerDealer
+{
+
+}
---
+++ src/test/java/com/architecture/assignment/ecommercestore/EcommerceStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.architecture.assignment.ecommercestore;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
