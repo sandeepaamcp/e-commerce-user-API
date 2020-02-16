fb6bb54c0c6dcfe40355b754bf5fc309db180711
sandeepa amcp <sandeepaamcp@gmail.com>
2020-02-16 21:42:26 +0530

    fixed cors issue for accessing endpoints

--- src/main/java/com/architecture/assignment/ecommercestore/mobileDevice/MobileManufacturer.java
+++ src/main/java/com/architecture/assignment/ecommercestore/mobileDevice/MobileManufacturer.java
@@ -2,5 +2,5 @@ package com.architecture.assignment.ecommercestore.mobileDevice;
 
 public enum MobileManufacturer
 {
-	SAMUSNG, APPLE, HUAWEI, HTC, MOTOROLA, NOKIA, XIAOMI, OPPO, LG, SONY
+	SAMSUNG, APPLE, HUAWEI, HTC, MOTOROLA, NOKIA, XIAOMI, OPPO, LG, SONY
 }
